d520f15db6a303179b8277397f562be4de29657b
Aarón Martínez Cuevas <github@roncuevas.com>
2026-02-11 09:34:47 -0600

    refactor: Add @MainActor to WebViewTaskManager

--- WebViewAMC/Sources/WebViewAMC/WebViewTaskManager.swift
+++ WebViewAMC/Sources/WebViewAMC/WebViewTaskManager.swift
@@ -1,44 +1,49 @@
 import Foundation
-import Combine
 
+@MainActor
 public final class WebViewTaskManager {
     public typealias TaskType = Task<(), any Error>
-    
+
     private var tasks = [String: TaskType]()
-    
+
     public func insert(key: String, value: TaskType) {
         tasks[key] = value
     }
-    
+
     public func remove(key: String) {
         tasks.removeValue(forKey: key)
     }
-    
+
     public var count: Int {
         tasks.count
     }
-    
+
+    public var keys: [String] {
+        tasks.keys.sorted()
+    }
+
     public func removeAll() {
         tasks.forEach { $0.value.cancel() }
         tasks.removeAll()
     }
-    
+
     public func remove(_ keys: [String]) {
         keys.forEach {
             tasks[$0]?.cancel()
             tasks.removeValue(forKey: $0)
         }
     }
-    
+
     public func getKeys() -> [String] {
-        tasks.keys.sorted()
+        self.keys
     }
-    
+
     public func hasTask(at key: String) -> Bool {
         tasks[key] != nil
     }
-    
-    public func getPublisher() -> Publishers.Sequence<[String : WebViewTaskManager.TaskType], Never> {
-        return tasks.publisher
+
+    public func awaitTask(key: String) async {
+        guard let task = tasks[key] else { return }
+        _ = try? await task.value
     }
 }
